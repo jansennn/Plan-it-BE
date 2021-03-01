87d784ce67d7f32d69af155f3a2682131412b4b7
Andreas  Tampubolon <38531347+jansennn@users.noreply.github.com>
2021-03-01 08:40:46 +0700

    update inisiasi

--- app/Http/Controllers/RutePerjalananController.php
+++ app/Http/Controllers/RutePerjalananController.php
@@ -8,12 +8,25 @@ use App\CategoryWisata;
 
 class RutePerjalananController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth:api']);
+    }
+
     public function inisiasi(){
-        $destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
+        //$destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
+        $destinasi = Destination::select('id')->pluck('id')->toArray();
+
+        for($i=0; $i<100;$i++){
+            shuffle($destinasi);
+            $contoh[$i] = $destinasi;
+            for($j=0; $j<count($contoh[$i]); $j++){
+                echo $contoh[$i][$j];
+            }
+            echo "<br>";
+        }
 
 
-
-
-        return $destinasi;
+        //return $destinasi;
     }
 }
