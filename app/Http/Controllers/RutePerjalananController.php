2c6e574b541ebd0a97b18f5d5621c73691c1f998
helmuth12081999 <helmuthsimon123@gmail.com>
2021-03-02 15:38:01 +0700

    perubahan

--- app/Http/Controllers/RutePerjalananController.php
+++ app/Http/Controllers/RutePerjalananController.php
@@ -14,19 +14,36 @@ class RutePerjalananController extends Controller
     }
 
     public function inisiasi(){
-        //$destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
-        $destinasi = Destination::select('id')->pluck('id')->toArray();
-
-        for($i=0; $i<100;$i++){
-            shuffle($destinasi);
-            $contoh[$i] = $destinasi;
-            for($j=0; $j<count($contoh[$i]); $j++){
-                echo $contoh[$i][$j];
+        $category = [1,2,3,4,5,6];
+        $destination = [];
+        if($category->count()>1){
+            $destination = $this->getDestinationByCategory(3);
+        }else{
+            for ($i=0; $i<$category->count();$i++){
+                foreach($this->getDestinationByCategory($category[$i]) as $d){
+                    array_push($destination , $d);
+                }
             }
-            echo "<br>";
         }
 
+        return $destination;
+        //$destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
 
-        //return $destinasi;
+
+//        for($i=0; $i<100;$i++){
+//            shuffle($destinasi);
+//            $contoh[$i] = $destinasi;
+//            for($j=0; $j<count($contoh[$i]); $j++){
+//                echo $contoh[$i][$j];
+//            }
+//            echo "<br>";
+//        }
+
+
+//        return $destinasi;
+    }
+
+    public function getDestinationByCategory($category){
+        return Destination::select('id')->where('category',$category)->get();
     }
 }
