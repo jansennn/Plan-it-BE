cd39a6b2307a3af8834e9104aa078264e6dd373f
helmuth12081999 <helmuthsimon123@gmail.com>
2021-03-03 17:06:49 +0700

    inisialisasi

--- app/Http/Controllers/RutePerjalananController.php
+++ app/Http/Controllers/RutePerjalananController.php
@@ -2,48 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Destination;
-use App\CategoryWisata;
 
 class RutePerjalananController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:api']);
+//        $this->middleware(['auth:api']);
     }
 
-    public function inisiasi(){
-        $category = [1,2,3,4,5,6];
-        $destination = [];
-        if($category->count()>1){
-            $destination = $this->getDestinationByCategory(3);
-        }else{
-            for ($i=0; $i<$category->count();$i++){
-                foreach($this->getDestinationByCategory($category[$i]) as $d){
-                    array_push($destination , $d);
-                }
+    public function inisiasi($category, $destination_length)
+    {
+//        untuk testing
+//        $category = [1, 2, 3, 4, 5, 6];
+        $destinations = [];
+//        untuk testing
+//        $destination_length = 7;
+        $population = [];
+
+//        menampung semua destinasi yang sesuai dengan kategori
+        for ($i = 0; $i < count($category); $i++) {
+            foreach ($this->getDestinationByCategory($category[$i]) as $d) {
+                array_push($destinations, $d['id']);
             }
         }
+//        mengisi kromosom kromosom ke dalam populasi
+        for ($i = 0; $i < 100; $i++) {
+//            mengacak posisi destinasi destinasi
+            shuffle($destinations);
 
-        return $destination;
-        //$destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
+//            mengambil 7 value pertama dari destination yang telah di acak
+            $chromosome = [];
+            for ($j = 0; $j < $destination_length; $j++) {
+                $chromosome[$j] = $destinations[$j];
+            }
+
+//            proses menampung kromosom, inisialisasi jarak tempuh, durasi perjalanan, rating, dan fitness
+            $population[$i] = array();
+            $population[$i]['chromosome'] = $chromosome;
+            $population[$i]['distance'] = 0;
+            $population[$i]['duration'] = 0;
+            $population[$i]['rating'] = 0;
+            $population[$i]['fitness'] = 0;
+        }
 
 
-//        for($i=0; $i<100;$i++){
-//            shuffle($destinasi);
-//            $contoh[$i] = $destinasi;
-//            for($j=0; $j<count($contoh[$i]); $j++){
-//                echo $contoh[$i][$j];
-//            }
-//            echo "<br>";
-//        }
-
-
-//        return $destinasi;
+        return $population;
     }
 
-    public function getDestinationByCategory($category){
-        return Destination::select('id')->where('category',$category)->get();
+    public function getDestinationByCategory($category)
+    {
+        return Destination::select('id')->where('category_wisata_id', $category)->get();
     }
 }
