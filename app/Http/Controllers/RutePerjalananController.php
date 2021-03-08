59bfbf240155dcf8d042dd25dfb9c37e0d452cb8
Andreas  Tampubolon <38531347+jansennn@users.noreply.github.com>
2021-03-08 16:04:04 +0700

    add table temp

--- app/Http/Controllers/RutePerjalananController.php
+++ app/Http/Controllers/RutePerjalananController.php
@@ -2,31 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Destination;
-use App\CategoryWisata;
 
 class RutePerjalananController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:api']);
+//        $this->middleware(['auth:api']);
     }
 
-    public function inisiasi(){
-        //$destinasi = Destination::select('id')->where(['category_wisata_id' => 1])->pluck('id')->toArray();
-        $destinasi = Destination::select('id')->pluck('id')->toArray();
+    public function inisiasi($category, $destination_length)
+    {
+//        untuk testing
+//        $category = [1, 2, 3, 4, 5, 6];
+        $destinations = [];
+//        untuk testing
+//        $destination_length = 7;
+        $population = [];
 
-        for($i=0; $i<100;$i++){
-            shuffle($destinasi);
-            $contoh[$i] = $destinasi;
-            for($j=0; $j<count($contoh[$i]); $j++){
-                echo $contoh[$i][$j];
+//        menampung semua destinasi yang sesuai dengan kategori
+        for ($i = 0; $i < count($category); $i++) {
+            foreach ($this->getDestinationByCategory($category[$i]) as $d) {
+                array_push($destinations, $d['id']);
             }
-            echo "<br>";
+        }
+//        mengisi kromosom kromosom ke dalam populasi
+        for ($i = 0; $i < 100; $i++) {
+//            mengacak posisi destinasi destinasi
+            shuffle($destinations);
+
+//            mengambil 7 value pertama dari destination yang telah di acak
+            $chromosome = [];
+            for ($j = 0; $j < $destination_length; $j++) {
+                $chromosome[$j] = $destinations[$j];
+            }
+
+//            proses menampung kromosom, inisialisasi jarak tempuh, durasi perjalanan, rating, dan fitness
+            $population[$i] = array();
+            $population[$i]['chromosome'] = $chromosome;
+            $population[$i]['distance'] = 0;
+            $population[$i]['duration'] = 0;
+            $population[$i]['rating'] = 0;
+            $population[$i]['fitness'] = 0;
         }
 
 
-        //return $destinasi;
+        return $population;
+    }
+
+    public function getDestinationByCategory($category)
+    {
+        return Destination::select('id')->where('category_wisata_id', $category)->get();
+    }
+
+
+
+    public function mutation(){
+
     }
 }
--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@
 ## 1 Setting data on DB
 - Run `php artisan migrate` to make table from migration.
 - Run `php artisan db::seed` to insert data from seeder to table DB.
-- Run `php artisan tinker` -> `factory(App\Destination::class, 20)->create()` to make data from factory
+- Run `php artisan tinker` -> `factory(App\Destination::class, 40)->create()` to make data from factory
 
